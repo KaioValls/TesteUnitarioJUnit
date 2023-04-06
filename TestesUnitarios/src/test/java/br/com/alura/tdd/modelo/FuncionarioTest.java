2f66382f0feb8a9d856f9d2653cd352be412034e
KaioValls <vallsbusiness.acc@gmail.com>
2023-04-06 14:53:01 -0300

    First and Last Commit

---
+++ TestesUnitarios/src/test/java/br/com/alura/tdd/modelo/FuncionarioTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.tdd.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FuncionarioTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
